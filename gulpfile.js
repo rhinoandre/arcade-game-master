2d38673eee091d62d7540fe228dfce40353fba4f
Rhino André <rhinoandre@gmail.com>
2018-04-02 20:24:52 -0300

    Watching JS files with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,28 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 browserSync.init({
-    server: {
-        baseDir: './'
-    }
+  server: {
+    baseDir: './'
+  }
 });
 
 gulp.task('default', () => {
-    gulp.watch('css/**/*.scss', ['style']);
+  gulp.watch('css/**/*.scss', ['style']);
+  gulp.watch('js/**/*.js', ['javascript']);
+});
+
+gulp.task('javascript', () => {
+  console.log('trt')
+  return gulp.src('js/**/*.js')
+    .pipe(browserSync.stream());
 });
 
 gulp.task('style', () => {
-    return gulp.src('css/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
+  return gulp.src('css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
